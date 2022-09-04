01c0a2947e3023ddb5199dc1cb1ede62be31d282
Jabbernaut <jabbernaut4@gmail.com>
2022-09-04 16:16:28 -0400

    9/4

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 import Locations from './components/Locations';
 import Contact from './components/Contact';
+import {Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
 			<Navbar />
-      <header className="Main-header">
-				Cincinnati Tourism
-      </header>
-			<Locations />
-			<Contact />
+			<Routes>
+			<Route exact path="/" element={<Home />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="/locations" element={<Locations />} />
+			</Routes>
 			<Footer />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 	return (
@@ -7,12 +8,12 @@ const Navbar = () => {
 	<label for="check" class="checkbtn">
 		<i class="fas fa-bars"></i>
 	</label>
-	<label class="logo">Discover <b>Cincinnati</b></label>
+	<label class="logo"><Link className="nav-link" to="/">Discover <b>Cincinnati</b></Link></label>
 	<ul>
-		<li><a class="active" href="/">Home</a></li>
-		<li><a href="/"><button type="button" class="btn btn-primary">Locations</button></a></li>
-		<li><a href="/">Map</a></li>
-		<li><a href="/">Contact</a></li>
+		<li><Link className="nav-link" to="/">Home</Link></li>
+		<li><Link className="nav-link" to="/locations">Locations</Link></li>
+		<li><Link className="nav-link" to="/map">Map</Link></li>
+		<li><Link className="nav-link" to="/contact">Contact</Link></li>
 	</ul>
 </nav>
 	)
