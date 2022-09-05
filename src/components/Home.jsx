50ca42758649c96beeef705a36363626a69cb486
Jabbernaut <jabbernaut4@gmail.com>
2022-09-05 17:12:37 -0400

    kayvon 9/5

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,13 @@ console.log(bgvideo);
 const Home = () => {
 	return (
 		<div>
-			<section className="showcase">
-    		<video className="fullscreen-video background" src={bgvideo} muted loop autoPlay></video>
+			<section className="my-video-container" id="section1">
+    		<video className="my-fullscreen-video my-background" src={bgvideo} muted loop autoPlay></video>
+				<h1 className="text-white my-tagline">A city full of history, culture, and fun!</h1>
   		</section>
+			<section className='my-center' id='section2'>
+				<h4 className='my-section2-tagline'>Come explore Cincinnati and discover all the hidden gems this city has to offer!</h4>
+			</section>
 		</div>
 	)
 }
