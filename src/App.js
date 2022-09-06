ed24667526085ed3669927fa228068878d3928e6
ksamavati <kayvonsamavati@gmail.com>
2022-09-06 13:34:15 -0400

    9/6 Kayvon API finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Subnav from './components/Subnav';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Breweries from './components/Breweries';
+import Category from './components/Category';
 import Locations from './components/Locations';
 import LocationsTest from './components/LocationsTest';
 import Contact from './components/Contact';
@@ -14,14 +16,16 @@ function App() {
     <div className="App">
 			<header>
 				<Navbar />
+				<Subnav />
 			</header>
 			<main>
 				<Routes>
 					<Route exact path="/" element={<Home />} />
 					<Route path="/contact" element={<Contact />} />
 					<Route path="/locations" element={<Locations />} />
-					<Route path="/locationstest" element={<LocationsTest />} />
 					<Route path="/breweries" element={<Breweries />} />
+					<Route path="/locationstest" element={<LocationsTest />} />
+					<Route path="/:cat" element={<Category />} />
 				</Routes>
 			</main>
 			<Footer />
