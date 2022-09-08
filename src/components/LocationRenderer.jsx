925ee4638e376e543a0c01b4f60f8f1d480ae4d3
ksamavati <kayvonsamavati@gmail.com>
2022-09-08 12:52:21 -0400

    Kayvon 9/8

--- src/components/LocationRenderer.jsx
+++ src/components/LocationRenderer.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import img1 from '../assets/ImgAttractions/cincinnati-zoo1.png';
 import { motion } from 'framer-motion';
 import '../css/Parallax.css';
 
 const LocationRenderer = (props) => {
 	return props.locationsArray.map(location => {
 		return (
-			// <div key={location.id} className='card my-3 mx-5' style={{ color: "inherit", textDecoration: "inherit" , maxWidth: "280px"}}>
-		// 	<motion.div
-		// 		layout
-		// 		animate={{opacity: 1}}
-		// 		initial={{opacity: 0}}
-		// 		exit={{opacity: 0}}
-		// 	 key={location.id} className='card my-3 mx-5' style={{ color: "inherit", textDecoration: "inherit" , 
-		// 	maxWidth: "350px"}}>
-		// 	<img className="card-img-top" src={img1} alt={"photo of " + location.name} style={{overflow: "hidden", objectFit: "cover"}} />
-		// 	<div className="card-body">
-		// 		<h5 className="card-title">{location.name}</h5>
-		// 		<p className="card-text">{location.description}</p>
-		// 		<button className="btn btn-primary">Read More &raquo;</button>
-		// 	</div>
-		// </motion.div>
 		<motion.div
 				layout
 				animate={{opacity: 1}}
@@ -28,7 +12,7 @@ const LocationRenderer = (props) => {
 				exit={{opacity: 0}}
 			 key={location.id} className='para-col lg-col-3' onTouchStart="this.classList.toggle('hover');">
 							<div class="para-container">
-					<div class="para-front" style={{backgroundImage: "url("+ img1 +")"}}>
+					<div class="para-front" style={{backgroundImage: "url("+ location.image +")"}}>
 						<div class="para-inner">
 							<p>{location.name}</p>
 							<span>{location.address1}</span>
--- src/components/LocationsTest.jsx
+++ src/components/LocationsTest.jsx
@@ -20,7 +20,7 @@ const LocationsTest = () => {
       <Carousel.Item interval={4000}>
         <img
           className="d-block w-100 my-car-img"
-          src="https://images.unsplash.com/photo-1557771834-305b440777de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+          src={"https://images.unsplash.com/photo-1557771834-305b440777de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"}
           alt="First slide"
         />
         <Carousel.Caption>
