b2db987306ea447243676c3d73f6c7a7d3947e70
Vera <veraopoku100@gmail.com>
2022-09-09 09:30:33 -0400

    hi

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
           <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
             <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
             <p className="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              What, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
               magna aliqua.
             </p>
             <ul>
