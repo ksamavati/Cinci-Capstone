e2f8af37225dfabaeba97dfbdb46b90c5e24a0eb
Jabbernaut <jabbernaut4@gmail.com>
2022-09-09 14:16:05 -0400

    Kayvon 9/9

--- src/components/OneLocation.jsx
+++ src/components/OneLocation.jsx
@@ -8,20 +8,47 @@ const OneLocation = () => {
 	const navigate = useNavigate();
 	const { loc } = location.state;
 
+
+
+	
 	// Runs once when compponent renders, equivalent to ComponentDidMount
 	// This will call Google Maps API
 	useEffect(() => {
-		if (loc === "" || loc === undefined) {
-			alert("Error: The app did not receive data for this location.");
-		} else {
-		axios.get('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJA-qv_um1QYgRAhCxo0Jd_1o')
-			.then(response => {
-				setMapsData(response.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-		}
+		const proxyurl = "https://cors-anywhere.herokuapp.com/";
+		const url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJA-qv_um1QYgRAhCxo0Jd_1o"; // site that doesn’t send Access-Control-*
+		fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+		.then(response => {setMapsData(response.data)})
+		.then(contents => console.log(contents))
+		.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+
+
+		// var config = {
+		// 	method: 'get',
+		// 	url: 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian&inputtype=textquery&locationbias=circle%3A2000%4047.6918452%2C-122.2226413&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=YOUR_API_KEY',
+		// 	headers: { }
+		// };
+		
+		// axios(config)
+		// .then(function (response) {
+		// 	console.log(JSON.stringify(response.data));
+		// })
+		// .catch(function (error) {
+		// 	console.log(error);
+		// });
+
+
+		// if (loc === "" || loc === undefined) {
+		// 	alert("Error: The app did not receive data for this location.");
+		// } else {
+		// axios.get('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJA-qv_um1QYgRAhCxo0Jd_1o')
+		// 	.then(response => {
+		// 		setMapsData(response.data);
+		// 	})
+		// 	.catch((error) => {
+		// 		console.log(error);
+		// 	});
+		// }
 	}, [loc]);
 
 	return (
