ced714e0e674b978e600ff889e7bfebfdb24c5eb
Vera <veraopoku100@gmail.com>
2022-09-11 14:24:42 -0400

    hi

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import '../css/Home.css';
 import bgvideo from '../assets/home/landingvideo.mp4';
+import movie from '../assets/home/videoplayback.mp4';
 import '../css/Home copy.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 
 console.log(bgvideo);
@@ -67,18 +71,18 @@ const Home = () => {
           </div>
 
           <div className="col-lg-3 col-6 text-center">
-            <span data-purecounter-start="0" data-purecounter-end="64" data-purecounter-duration="1" className="purecounter"></span>
-            <p>Courses</p>
+            <span data-purecounter-start="0" data-purecounter-end="80" data-purecounter-duration="1" className="purecounter"></span>
+            <p>Breweries</p>
           </div>
 
           <div className="col-lg-3 col-6 text-center">
             <span data-purecounter-start="0" data-purecounter-end="42" data-purecounter-duration="1" className="purecounter"></span>
-            <p>Events</p>
+            <p></p>
           </div>
 
           <div className="col-lg-3 col-6 text-center">
             <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" className="purecounter"></span>
-            <p>Trainers</p>
+            <p></p>
           </div>
 
         </div>
@@ -87,22 +91,145 @@ const Home = () => {
     </section>
 		{/* <!-- End Counts Section --> */}
 
+            <section>
+            <Button variant="secondary"><h1>Things to Do</h1></Button>{' '}
+            <CardGroup>
+      <Card>
+        <Card.Img variant="top" src="assets/img/sport.jpeg" />
+        <Card.Body>
+          <Card.Title>Attractions and Sports</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        {/* <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer> */}
+        <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        {/* <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text> */}
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="assets/img/bar.png" />
+        <Card.Body>
+          <Card.Title>Bars and Breweries</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        {/* <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer> */}
+        <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        {/* <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text> */}
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="assets/img/art.jpg" />
+        <Card.Body>
+          <Card.Title>Performing Arts and Music</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        {/* <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer> */}
+        <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        {/* <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text> */}
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="assets/img/attract.jpeg" />
+        <Card.Body>
+          <Card.Title>Parks and Recreation</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        {/* <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer> */}
+        <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        {/* <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text> */}
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Card>
+    </CardGroup>
+
+    
+            </section>
     {/* <!-- ======= Why Us Section ======= --> */}
-    <section id="why-us" className="why-us">
+    <section>
+    <div>
+          <h2>Why Experience Cincinnati?</h2>
+          <video className="" src={movie} muted loop autoPlay></video>
+          <p>
+            
+Cincinnati is a great city with a lot to offer. The people are friendly and there is always something to do. The city has a great history and there are plenty of places to visit. The food is amazing and there is a great variety of restaurants. The city is also home to a great music scene. There are many reasons to visit Cincinnati, but these are just a few of the reasons why you should experience Cincinnati for yourself.
+          </p>
+          </div>
+          
+    </section>
+    {/* <section id="why-us" className="why-us">
       <div className="container" data-aos="fade-up">
 
         <div className="row">
           <div className="col-lg-4 d-flex align-items-stretch">
             <div className="content">
-              <h3>Why Choose Mentor?</h3>
+              <h3>Why Experience Cincinnati?</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
-                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
+              Cincinnati is a great city with a lot to offer. The people are friendly and there is always something to do. The city has a great history and there are plenty of places to visit. The food is amazing and there is a great variety of restaurants. The city is also home to a great music scene. There are many reasons to visit Cincinnati, but these are just a few of the reasons why you should experience Cincinnati for yourself.
               </p>
               <div className="text-center">
                 <a href="about.html" className="more-btn">Learn More <i className="bx bx-chevron-right"></i></a>
               </div>
             </div>
+          </div>
+          <div>
+          <h2>Why Experience Cincinnati?</h2>
+          <video className="" src={movie} muted loop autoPlay></video>
           </div>
           <div className="col-lg-8 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
             <div className="icon-boxes d-flex flex-column justify-content-center">
@@ -130,53 +257,53 @@ const Home = () => {
                 </div>
               </div>
             </div>
-						{/* <!-- End .content--> */}
+				
           </div>
         </div>
 
       </div>
-    </section>
+    </section> */}
 		{/* <!-- End Why Us Section --> */}
 
     {/* <!-- ======= Features Section ======= --> */}
-    <section id="features" className="features">
+    {/* <section id="features" className="features">
       <div className="container" data-aos="fade-up">
 
         <div className="row" data-aos="zoom-in" data-aos-delay="100">
           <div className="col-lg-3 col-md-4">
             <div className="icon-box">
               <i className="ri-store-line" style={{color: "#ffbb2c"}}></i>
-              <h3><a href="">Lorem Ipsum</a></h3>
+              <h3><a href="">Attractions</a></h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-4 mt-4 mt-md-0">
             <div className="icon-box">
               <i className="ri-bar-chart-box-line" style={{color: "#5578ff"}}></i>
-              <h3><a href="">Dolor Sitema</a></h3>
+              <h3><a href="">Sports</a></h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-4 mt-4 mt-md-0">
             <div className="icon-box">
               <i className="ri-calendar-todo-line" style={{color: "#e80368"}}></i>
-              <h3><a href="">Sed perspiciatis</a></h3>
+              <h3><a href="">Bars</a></h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-4 mt-4 mt-lg-0">
             <div className="icon-box">
               <i className="ri-paint-brush-line" style={{color: "#e361ff"}}></i>
-              <h3><a href="">Magni Dolores</a></h3>
+              <h3><a href="">Breweries</a></h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
             <div className="icon-box">
               <i className="ri-database-2-line" style={{color: "#47aeff"}}></i>
-              <h3><a href="">Nemo Enim</a></h3>
+              <h3><a href="">Performing Arts</a></h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
             <div className="icon-box">
               <i className="ri-gradienter-line" style={{color: "#ffa76e"}}></i>
-              <h3><a href="">Eiusmod Tempor</a></h3>
+              <h3><a href="">Parks and Recreation</a></h3>
             </div>
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
@@ -218,11 +345,11 @@ const Home = () => {
         </div>
 
       </div>
-    </section>
+    </section> */}
 		{/* <!-- End Features Section --> */}
 
     {/* <!-- ======= Popular Courses Section ======= --> */}
-    <section id="popular-courses" className="courses">
+    {/* <section id="popular-courses" className="courses">
       <div className="container" data-aos="fade-up">
 
         <div className="section-title">
@@ -257,7 +384,7 @@ const Home = () => {
               </div>
             </div>
           </div> 
-					{/* <!-- End Course Item--> */}
+					
 
           <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
             <div className="course-item">
@@ -284,7 +411,7 @@ const Home = () => {
               </div>
             </div>
           </div> 
-					{/* <!-- End Course Item--> */}
+				
 
           <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
             <div className="course-item">
@@ -311,16 +438,16 @@ const Home = () => {
               </div>
             </div>
           </div> 
-					{/* <!-- End Course Item--> */}
+				
 
         </div>
 
       </div>
-    </section>
+    </section> */}
 		{/* <!-- End Popular Courses Section --> */}
 
     {/* <!-- ======= Trainers Section ======= --> */}
-    <section id="trainers" className="trainers">
+    {/* <section id="trainers" className="trainers">
       <div className="container" data-aos="fade-up">
 
         <div className="row" data-aos="zoom-in" data-aos-delay="100">
@@ -384,9 +511,15 @@ const Home = () => {
         </div>
 
       </div>
-    </section>
+    </section> */}
 		{/* <!-- End Trainers Section --> */}
-
+<section>
+  <h1>Excited? Plan Your Trip With Us!</h1>
+  <div className="col-md-6 order-1 order-md-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/img/travel.jpg" className="img-fluid" alt="" />
+          </div>
+  <Button variant=""><Card.Link href="https://reservations.cincinnatiusa.com/hotel/list/4872/m2441?Search%5BhotelRegion%5D=m2441&Search%5BcheckInDate%5D=&Search%5BcheckOutDate%5D=&Search%5BnumberOfRooms%5D=1&Search%5BnumberOfAdults%5D=1&Search%5BnumberOfChildren%5D=0&Search%5BkeywordSearch%5D=&Search%5Bsort%5D=rank&Search%5BstarRating%5D=&Search%5BlowPrice%5D=&Search%5BhighPrice%5D=&customNavLink=0&Search%5Bnear%5D=&Search%5Blatitude%5D=&Search%5Blongitude%5D=&Search%5BlocationSearch%5D=Downtown&Search%5BpoiDistance%5D="><h3>Click Me</h3></Card.Link></Button>
+</section>
   </main>
 	{/* <!-- End #main --> */}
 
@@ -398,13 +531,19 @@ const Home = () => {
         <div className="row">
 
           <div className="col-lg-3 col-md-6 footer-contact">
-            <h3>Mentor</h3>
+            <h3>Contact Info</h3>
             <p>
-              A108 Adam Street <br />
+             <p>City of Cincinnati</p>
+      <p>8th Floor, Room 803</p>
+      <p>801 Plum Street</p>
+      <p>Cincinnati, Ohio 45202</p>
+      <p><strong>Phone:</strong> (513) 352-3120</p>
+      <strong>Email:</strong> ouremail@gmail.com<br />
+      <p>Monday - Friday, 8 a.m. - 5 p.m.</p>
+      <a href="https://www.cincinnati-oh.gov/">Cincinnati-oh.gov</a><br />
+              {/* A108 Adam Street <br />
               New York, NY 535022<br />
-              United States <br /><br />
-              <strong>Phone:</strong> +1 5589 55488 55<br />
-              <strong>Email:</strong> info@example.com<br />
+              United States <br /><br /> */}
             </p>
           </div>
 
@@ -412,14 +551,14 @@ const Home = () => {
             <h4>Useful Links</h4>
             <ul>
               <li><i className="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i className="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i className="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i className="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i className="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+              <li><i className="bx bx-chevron-right"></i> <a href="#">Locations</a></li>
+              <li><i className="bx bx-chevron-right"></i> <a href="#">Map</a></li>
+              <li><i className="bx bx-chevron-right"></i> <a href="#">Privacy Policy</a></li>
+              <li><i className="bx bx-chevron-right"></i> <a href="#">Terms Of Use</a></li>
             </ul>
           </div>
 
-          <div className="col-lg-3 col-md-6 footer-links">
+          {/* <div className="col-lg-3 col-md-6 footer-links">
             <h4>Our Services</h4>
             <ul>
               <li><i className="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
@@ -428,7 +567,7 @@ const Home = () => {
               <li><i className="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
               <li><i className="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
             </ul>
-          </div>
+          </div> */}
 
           <div className="col-lg-4 col-md-6 footer-newsletter">
             <h4>Join Our Newsletter</h4>
